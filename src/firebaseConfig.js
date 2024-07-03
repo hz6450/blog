d235560787baed78dbbdb37fb34cdb5ec1ea8a14
hj645000 <hj645000@gmail.com>
2024-07-03 17:38:43 +0900

    .env설정

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,15 +5,17 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDhZ31NZWyvXrtjLXQ-FfC4KyewRjMtzT4",
-  authDomain: "vercel-1c1e6.firebaseapp.com",
-  projectId: "vercel-1c1e6",
-  storageBucket: "vercel-1c1e6.appspot.com",
-  messagingSenderId: "822921920631",
-  appId: "1:822921920631:web:248a65d9b818a57f5d0672"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export default app;
+
+
